b975128cc4ba1eb7bd5d123b9aa3318b870b01fa
Va4i4o <mrengoy2@gmail.com>
2024-08-10 20:23:11 +0300

    Add files via upload

---
+++ 1 st homework.py
@@ -0,0 +1,4 @@
+print (9 ** 0.5 * 5) # 1 st program
+print ( 9.99 > 9.98 and 1000 != 1000.1) # 2 st program
+print ( (2 * 2 + 2) == ( 2 * ( 2+2 )) ) # 3 st program
+print ((int((float('123.456')*10)))% 10) # 4th program
