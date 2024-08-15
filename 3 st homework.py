c72a224bf96c47247f69a9e90f790cfc3598930e
Va4i4o <mrengoy2@gmail.com>
2024-08-15 10:50:53 +0300

    Add files via upload

---
+++ 3 st homework.py
@@ -0,0 +1,5 @@
+kolvo_dz = '12'
+kolvo_zatrat_4acov = '1.5'
+name_kurc = 'Python,'
+vremia_v_4ace = '60'
+print (name_kurc, 'всего задач:', kolvo_dz, 'затрачено часов:', kolvo_zatrat_4acov, 'среднее время выполнения ', (float(kolvo_zatrat_4acov) * int(vremia_v_4ace)/int(kolvo_dz))/int(vremia_v_4ace))
