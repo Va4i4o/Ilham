cf99b7d708e5b70e3689e40a6e73d620adc0b1a0
Va4i4o <mrengoy2@gmail.com>
2024-08-19 20:29:15 +0300

    Add files via upload

---
+++ my_module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Чина': 1997, 'Альпачина ': 1999}
+print (my_dict)
+print (my_dict.get('Чина'))
+print (my_dict.get('Ильхам'))
+my_dict.update({'Ли': 2001, 'Мерси': 2002 })
+a = my_dict.pop ('Ли')
+print (a)
+print (my_dict)
+my_set = {1,1,'vilka', 'vilka', True, True}
+print (my_set)
+print (my_set.add(6))
+print (my_set.add(7))
+print (my_set)
+print (my_set.remove('vilka'))
+print (my_set)
